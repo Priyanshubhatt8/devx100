88f1c979929e674002725bf119aca1f1ee59a01d
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2024-01-16 12:41:38 +0530

    week5

--- week5/week5.1/reactjs/src/App.jsx
+++ week5/week5.1/reactjs/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([
+    {
+      name: "Priyanshu",
+      title: "Study",
+      age: 12,
+    },
+    {
+      name: "Akash mishra",
+      title: "Playgames",
+      age: 10,
+    },
+    {
+      name: "Shubham mishra",
+      title: "study",
+      age: 11,
+    },
+  ]);
+  function newtodo() {
+    setTodo([
+      ...todo,
+      {
+        name: "New priyanshu",
+        title: "Study",
+        age: "10",
+      },
+    ]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={newtodo}> Add a random todo</button>
+
+      {todo.map(function (todo) {
+        return <Puttodo name={todo.name} title={todo.title}></Puttodo>;
+      })}
+    </div>
+  );
 }
 
-export default App
+function Puttodo(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.title}</h2>
+    </div>
+  );
+}
+
+export default App;
