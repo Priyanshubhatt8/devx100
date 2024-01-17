7395151775c3a5512645ea1e9bbc90bfdc0857d7
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2024-01-17 13:13:33 +0530

    week5

--- week5/week5.3/Project/Backend/index.js
+++ week5/week5.3/Project/Backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const app = express();
 const port = 6777;
-const { createTodo, updateTodo } = require("./types")
+const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 app.use(express.json());
 
 
 
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
 
     const payload = req.body
     const parsedpayload = createTodo.safeParse(payload)
@@ -18,16 +19,31 @@ app.post('/todo', (req, res) => {
         })
         return;
     }
+    await todo.create({
+        title: payload.title,
+        description: payload.description,
+        completed: false
+    })
+    res.json({
+        msg: "toto created successfully"
+    })
     // put it in mongodb
 })
 
 
-app.get('/todos', (req, res) => {
+app.get('/todos', async (req, res) => {
+
+    const todos = await todo.find({})
+    console.log(todo)
+
+    res.json({
+        todos
+    })
 
 })
 
 
-app.put('/completed', (req, res) => {
+app.put('/completed', async (req, res) => {
 
     const updateid = req.body
     const checkid = updateTodo.safeParse(updateid)
@@ -36,8 +52,18 @@ app.put('/completed', (req, res) => {
         res.status(411).json({
             msg: "Wrong inputs"
         })
+        return;
     }
-
+    await todo.update({
+        _id: req.body.id
+    },
+        {
+            completed: true
+        }
+    )
+    res.json({
+        msg: "Todo has been updated "
+    })
 })
 
 
