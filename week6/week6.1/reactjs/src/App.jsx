fa1d47cd1bf393f6fa9285796820fe01342ee056
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2024-01-19 01:49:57 +0530

    week6

--- week6/week6.1/reactjs/src/App.jsx
+++ week6/week6.1/reactjs/src/App.jsx
@@ -1,12 +1,21 @@
+// import { useState } from "react";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  function increament() {
+    setCount(count + 1);
+  }
+  function Decreament() {
+    setCount(count - 1);
+  }
   return (
     <div>
-      <h1>Hi therwe</h1>
+      <h1>{count}</h1>
+      <button onClick={increament}>Increment</button>
+      <button onClick={Decreament}>Decreament</button>
     </div>
   );
 }
