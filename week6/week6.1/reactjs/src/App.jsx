256c0ca497dd1f1f7de95d7bb189d256a8696bf3
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2024-01-22 10:49:18 +0530

    week6

--- week6/week6.1/reactjs/src/App.jsx
+++ week6/week6.1/reactjs/src/App.jsx
@@ -1,39 +1,105 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import React from "react";
+
 function App() {
-  return (
-    <>
-      <CardWrapper innerComponent={<TextComponent />}></CardWrapper>
-      <CardWrapper innerComponent={<TextComponent2 />}></CardWrapper>
-    </>
-  );
-}
+  const [todos, setTodos] = useState([]);
 
-function CardWrapper({ innerComponent }) {
-  return (
-    <>
-      <div style={{ border: "2px solid red", padding: "20x" }}>
-        <h3>{innerComponent}</h3>
-      </div>
-    </>
-  );
-}
+  useEffect(() => {
+    setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos").then(async function (res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      });
+    }, 1000);
+  }, []);
 
-function TextComponent() {
   return (
     <div>
-      <h1>Hi there</h1>
+      {todos.map((todo) => {
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          description={todo.description}
+        ></Todo>;
+      })}
     </div>
   );
 }
-
-function TextComponent2() {
+function Todo({ title, description }) {
   return (
     <div>
-      <h1>Hi there2</h1>
+      <h1>{title}</h1>
+      <h2>{description}</h2>
     </div>
   );
 }
 export default App;
+
+// function App() {
+//   const [title, setTitle] = useState("My name is ");
+
+//   function changeme() {
+//     setTitle("Your name is priyanshu bhatt" + Math.random());
+//   }
+
+//   return (
+//     <>
+//       <div>
+//         <button onClick={changeme}>Change me</button>
+//         <Header title={title}></Header>
+//         <Header title="Hi welcome to this props"></Header>
+//         <Header title="Hi welcome to this props"></Header>
+//         <Header title="Hi welcome to this props"></Header>
+//         <Header title="Hi welcome to this props"></Header>
+//         <Header title="Hi welcome to this props"></Header>
+//       </div>
+//     </>
+//   );
+// }
+// export default App;
+
+// const Header = React.memo(function Header({ title }) {
+//   return (
+//     <div>
+//       <div>{title}</div>
+//     </div>
+//   );
+// });
+
+// function App() {
+//   return (
+//     <>
+//       <CardWrapper>hi there</CardWrapper>
+//     </>
+//   );
+// }
+
+// function CardWrapper({ children }) {
+//   return (
+//     <>
+//       <div style={{ border: "2px solid red", padding: "20x" }}>
+//         <h3>{children}</h3>
+//       </div>
+//     </>
+//   );
+// }
+
+// function TextComponent() {
+//   return (
+//     <div>
+//       <h1>Hi there</h1>
+//     </div>
+//   );
+// }
+
+// function TextComponent2() {
+//   return (
+//     <div>
+//       <h1>Hi there2</h1>
+//     </div>
+//   );
+// }
+// export default App;
 
 // const [todos, setTodos] = useState([
 //   {
